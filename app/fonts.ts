1923e26cc77a742f0621c274db68ba56ac73da39
berkaycirak <berkaycirak_11@hotmail.com>
2024-07-11 08:46:17 +0300

    first commit

---
+++ app/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Nunito, Reddit_Mono } from 'next/font/google';
+
+export const nunito = Nunito({
+	subsets: ['latin'],
+});
+export const redditMono = Reddit_Mono({
+	subsets: ['latin'],
+});
